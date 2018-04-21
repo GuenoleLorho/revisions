760cc1c40b281e3145d9357fc93eb899a2e557c2
guenole lorho <lorho.guenole@gmail.com>
2018-04-21 23:06:19 +0100

    lab 13 ...modules

--- lab13/mysecondserver.js
+++ lab13/mysecondserver.js
@@ -6,9 +6,8 @@ http.createServer(function (req, res) {
 
 var http = require('http');
 var currentdate = require('./mymodule');
-
-http.createServer(function (req, res){
-  res.writeHead(200, {'Content-type': 'text/html'});
-  res.write("the date and time are currently: " + currentdate.myDateTime());
-  res.end('Hello World!');
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write("The date and time are currently: " + currentdate.myDateTime());
+ res.end('Hello World!');
 }).listen(8080);
