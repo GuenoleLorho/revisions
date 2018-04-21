1e9eb994d9c5cf495eba607d99f40c6cbe16e83a
guenole lorho <lorho.guenole@gmail.com>
2018-04-21 23:02:59 +0100

    lab 13 modules try n°5, typo n°4

--- lab13/mysecondserver.js
+++ lab13/mysecondserver.js
@@ -10,5 +10,5 @@ var currentdate = require('./mymodule');
 http.createServer(function (req, res){
   res.writeHead(200, {'Content-type': 'text/html'});
   res.write("the date and time are currently: " + currentdate.myDateTime());
-  res.end('Hello World!')
+  res.end('Hello World!');
 }).listen(8080);
