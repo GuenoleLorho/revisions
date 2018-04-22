7fbbdfd77c672840ed8c1e37a74e4e36fd86d04e
guenole lorho <lorho.guenole@gmail.com>
2018-04-22 09:50:36 +0100

    lab13 express joke page

--- lab13/server.js
+++ lab13/server.js
@@ -7,4 +7,9 @@ app.get('/', function(req, res){
 app.get('/test', function(req, res){
   res.send("this is route 2");
 })
+app.get('/joke', function(req, res){
+  var knockknock = require ('knock-knock-jokes')
+  var randomJoke = knockknock()
+  res.end(randomJoke);
+})
 app.listen(8080);
