b75a6ea737ab602b7612014311bbeac706c5db16
guenole lorho <lorho.guenole@gmail.com>
2018-04-22 09:43:01 +0100

    lab13 add second route to express

--- lab13/server.js
+++ lab13/server.js
@@ -4,4 +4,7 @@ var app = express();
 app.get('/', function(req, res){
   res.send("hello world! by express");
 })
+app.get('/test', function(req, res){
+  res.send("this is route 2");
+})
 app.listen(8080);
