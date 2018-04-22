1bcad55b4dfa8ec08d88c83aa1fd1a5c6121fa1d
guenole lorho <lorho.guenole@gmail.com>
2018-04-22 09:00:04 +0100

    lab 13 modules...

--- lab13/mymodule.js
+++ lab13/mymodule.js
@@ -1,6 +1,4 @@
-
-//We use the exports keyword to tell node this is a function that other code can
-//use.
-exports.myDateTime = function(){
+//We use the exports keyword to tell node this is a function that other code can use.
+exports.myDateTime = function() {
   return Date();
 };
