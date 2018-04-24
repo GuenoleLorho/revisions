abcc1df8e20f4f06110863524b9614649bfdb0b6
guenole lorho <lorho.guenole@gmail.com>
2018-04-25 00:21:16 +0100

    lab15 start wars quote (typo fixed)

--- lab15/server.js
+++ lab15/server.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/star_wars-quotes";
+const url = "mongodb://localhost:27017/star_wars_quotes";
 const express = require('express');
 const app = express();
 
