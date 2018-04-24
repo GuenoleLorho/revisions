672c484e01e6cca6685c9c7b2f4bab216e43b725
guenole lorho <lorho.guenole@gmail.com>
2018-04-24 08:40:30 +0100

    lab 14 getting tweets test

--- lab14/server.js
+++ lab14/server.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var app =express();
+
+var Twitter = require('twitter');
+var client = new Twitter({
+  consumer_key: 'xxxxxxxx',
+  consumer_secret: 'xxxxxxx',
+  access_token_key: 'xxxxxxx',
+  access_token_secret: 'xxxxxxxx'
+})
+
 app.use(express.static('public'))
+
+var params = {screen_name: 'node.js'};
+client.get('statues/user_timeline', params, function(error, tweets, respond){
+  if(!error){
+    console.log(tweets);
+  }
+});
 
 app.get('/', function(req, res){
   res.send("Hello world! by express");
 });
+
+
 
 app.listen(8080);
