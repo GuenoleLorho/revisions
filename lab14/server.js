7157325f79c0c1f57b174bea9988b2b276ab42d8
guenole lorho <lorho.guenole@gmail.com>
2018-04-24 17:20:09 +0100

    lab 14 twitter init

--- lab14/server.js
+++ lab14/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = express();
+var app = express();/*
 var Twitter = require('twitter');
 
 var client = new Twitter({
@@ -7,14 +7,14 @@ var client = new Twitter({
   consumer_secret: 'UIXD2rBiFvGEiqYCW1Gv1JgcQikOMLpfTwC2NHx5DbwaJhvRJX',
   access_token_key: '988554737120432128-ekr9I8PrMWzWYuzSVOKH4xztT2kqxSR',
   access_token_secret: 'mkrYHaXlJ7MXhUZNB5fAkbjOEzvHxWZk33UbYM1oSG27A'
-});
+});*/
 
 
 app.use(express.static('public'))
 
 
 
-app.get('/tweetsjson', function(req, res) {
+app.get('/', function(req, res) {/*
   var params = {
     screen_name: 'nodejs'
   };
@@ -22,7 +22,7 @@ app.get('/tweetsjson', function(req, res) {
     if (!error) {
       console.log("tweets"):
     }
-  });
+  });*/
 
   res.send("hello world! by express");
 });
