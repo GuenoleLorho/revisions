cf94fcbab48965b24efce3339479ade6bf41a33f
guenole lorho <lorho.guenole@gmail.com>
2018-04-24 17:26:05 +0100

    lab14 read tweet (typo fixed)

--- lab14/server.js
+++ lab14/server.js
@@ -20,7 +20,7 @@ app.get('/', function(req, res) {
   };
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-      console.log("tweets"):
+      console.log("tweets");
     }
   });
 
