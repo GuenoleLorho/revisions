f08c48821d5aa684a389cff4ca5f066ea343e657
guenole lorho <lorho.guenole@gmail.com>
2018-04-25 12:16:12 +0100

    lab17-quotes ...

--- lab17-quotes/server.js
+++ lab17-quotes/server.js
@@ -48,12 +48,12 @@ app.get('/update', function(req,res) {
 
 
 
-app.get('/', function(req, res) {
+app.get('/Allquotes', function(req, res) {
   db.collection('quotes').find().toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
 
-    res.render('pages/index', {
+    res.render('index', {
       quotesarray : result
     });
   });
