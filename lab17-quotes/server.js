3ac7dd77e99ce66732f875701c80abe9e88924ae
guenole lorho <lorho.guenole@gmail.com>
2018-04-25 13:26:15 +0100

    lab17-quotes filter+results

--- lab17-quotes/server.js
+++ lab17-quotes/server.js
@@ -94,7 +94,7 @@ app.post('/search', function(req, res) {
     if (err) throw err;
 
     var output = "<h1>quotes by" +req.body.name+ "</h1>";
-    res.send('pages/filter', {
+    res.send('pages/results', {
       quotesarray : result,
       output : output
     });
