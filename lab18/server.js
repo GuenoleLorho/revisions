acfbbf9d82d9b033dfdcfb995cc774775757b891
guenole lorho <lorho.guenole@gmail.com>
2018-04-25 16:28:17 +0100

    lab18 main user db test

--- lab18/server.js
+++ lab18/server.js
@@ -94,7 +94,7 @@ app.get('/', function(req, res) {
     //console.log(uname+ ":" + result);
     //finally we just send the result to the user page as "user"
     res.render('pages/users', {
-      user: result
+      mainuser: result
     })
   });
   console.log("string = "+string)
